5e6c3d6937e8ded23721fe1853fce8abe8b91499
Adeel Miraj <adeelmiraj@gmail.com>
2016-11-15 20:01:42 +0500

    Initial Commit

---
+++ Full ProfileTests/Full_ProfileTests.swift
@@ -0,0 +1,36 @@
+//
+//  Full_ProfileTests.swift
+//  Full ProfileTests
+//
+//  Created by Adeel Miraj on 15/11/2016.
+//  Copyright © 2016 AM Co. All rights reserved.
+//
+
+import XCTest
+@testable import Full_Profile
+
+class Full_ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
